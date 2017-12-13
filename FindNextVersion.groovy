8658fe5c8fba02476ae6c2042f42474a2c639f48
Mika Nokka <mika.nokka@ambientia.fi>
2017-12-13 11:56:04 +0200

    Works ok, Selects first avaialble version and sets it to Create screen

--- FindNextVersion.groovy
+++ FindNextVersion.groovy
@@ -7,7 +7,7 @@
 // Dec 2017    mika.nokka1@gmail.com   
 
 
-package com.onresolve.jira.groovy.doit  // this script must be living under this tree starting from /scripts directory (<jira place>/scripts/com/resolve....)
+package com.onresolve.jira.groovy.doit2  // this script must be living under this tree starting from /scripts directory (<jira place>/scripts/com/resolve....)
 import com.atlassian.jira.component.ComponentAccessor
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
@@ -15,23 +15,41 @@ import org.apache.log4j.Level
 import com.atlassian.jira.issue.Issue
 import com.atlassian.jira.issue.MutableIssue
 import com.onresolve.jira.groovy.user.FieldBehaviours   // class to be used if script in server
+import com.atlassian.jira.issue.IssueManager
 
-public class FieldHider extends FieldBehaviours {
+import static com.atlassian.jira.issue.IssueFieldConstants.*  // can use things like AFFECTED_VERSIONS
 
-	void Doit() {	// just a method runtime system is calling (used in Behaviours configurations)
+public class FindNextVersions extends FieldBehaviours {
+
+void Doit2() {	// just a method runtime system is calling (used in Behaviours configurations)
+	
+		def log = Logger.getLogger("FindNextVersion")
+		log.setLevel(Level.DEBUG) // DEBUG INFO
+	
+		log.info("---------- FindNextVersion started ---------------------------------------")
+		
+
+	
+		//def issue=underlyingIssue // from ScriptRunner example code
+		
+	
 		
 		def versionManager = ComponentAccessor.getVersionManager()
 		def projectManager = ComponentAccessor.getProjectManager()
-		def project = projectManager.getProjectObjByKey(issue.projectObject.key)
+		def project = projectManager.getProjectObjByKey(issueContext.projectObject.key)
 		def versions = versionManager.getVersions(project)
+		def issueManager = ComponentAccessor.getIssueManager()
+		
+		// Of course issue does not exists, this code colled when create screen is opened
+		//def issuekey=underlyingIssue // from ScriptRunner example code		
+		// Should use Behaviours way to get, this is standard
+		//Issue issue = issueManager.getIssueObject("${issuekey}")  // use really as a string
 		def newversions = versions.collect()
-  
-		def log = Logger.getLogger("FindNextVersion")
-		log.setLevel(Level.INFO) // DEBUG INFO
-  
-		log.info("---------- FindNextVersion started ---------------------------------------")
-		def Project=issue.projectObject.name
-		newversions = newversions.sort({version1, version2 -> version1.releaseDate<=>version2.releaseDate}).findAll{version -> ! version.released }
+		//log.info("issuekey:${issuekey}  issue:${issue}")
+
+		def issue="NOT_EXISTS"
+		def Project="NOT_SET"
+newversions = newversions.sort({version1, version2 -> version1.releaseDate<=>version2.releaseDate}).findAll{version -> ! version.released }
 
 
 		if (newversions) {
@@ -41,10 +59,11 @@ public class FieldHider extends FieldBehaviours {
 			def versionToUse = newversions.first();
 	
 	
-			MutableIssue myIssue = issue
-			myIssue.setFixVersions([versionToUse])
-			myIssue.store() // needed to store changes
-			log.info("Set version:${versionToUse} as fixed version for issue:${issue}")
+			//MutableIssue myIssue = issue
+			//myIssue.setFixVersions([versionToUse])
+			//myIssue.store() // needed to store changes
+			getFieldById(FIX_FOR_VERSIONS).setFormValue([versionToUse.id])
+			log.info("WOULD LIKE TO Set version:${versionToUse} as fixed version for issue:${issue}")
 		}
 
 		else {
